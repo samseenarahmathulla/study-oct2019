c89a007c543d9ade0b9a8e93deda65067a7a9900
samseena <samseenar@gmail.com>
2019-10-21 09:42:35 +0800

    rspec test cases for new and create method

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -26,4 +26,45 @@ RSpec.describe ArticlesController, type: :controller do
     end
   end
 
+  context 'GET #new' do
+    # except index and show all other controller methods needs to be authenticated.
+    before(:each) do
+      admin_login('sam', 'sam')
+    end
+    it 'returns a success if http authentication is done' do
+      get :new
+      expect(response).to have_http_status(:ok) #200 as we already authenticated using admin_login else it would be 401 error if authentication not done.
+    end
+    it 'render new template view if authorized' do
+      get :new
+      expect(response).to render_template("new")
+      expect(response.body).to eq ""
+    end
+  end
+
+
+  context 'GET #create' do
+    before(:each) do
+      admin_login('sam', 'sam')
+    end
+
+    #TODO: find the right way to pass the params to controller methods
+    # it 'render index page if article is created' do
+    #   article  = Article.create!(title: 'Test title', text: 'Test text test text')
+    #   get :create, params: { article.to_param}
+    #   expect(response).to render_template("index")
+    # end
+    # it 'render new page if article is not created' do
+    #   article  = Article.create(title: 'Test', text: 'Test text test text')
+    #   get :create, params: { title: 'Test', text: 'Test text test text'}
+    #   expect(response).to render_template("new")
+    # end
+
+  end
+
+  private
+  def admin_login(user_name, password)
+    request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Basic.encode_credentials(user_name, password)
+  end
+
 end
